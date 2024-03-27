7548ef4f0565df2be408de1097f5a27404283420
JeongSunghan <jsh000311@gmail.com>
2024-03-27 12:24:46 +0900

    first commit

---
+++ src/pratice/ch02.js
@@ -0,0 +1,14 @@
+let s = `He said "I am unhappy."`;    // template literal
+console.log(s);
+
+let year = new Date().getFullYear();
+console.log(`올해는 ${year}년 입니다.`);
+
+let toggle = true;
+for (let i = 0; i < 4; i++) {
+  toggle = !toggle;
+  console.log(toggle);
+}
+
+console.log(`2 == '2' is ${2 == '2'}`);     // true, 값은 같음
+console.log(`2 === '2' is ${2 === '2'}`);   // false, 값은 같으나 자료형이 다름
